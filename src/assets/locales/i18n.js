c2132c371fca32b93b2d7681684463e71b78f047
Karol Tatała <ktatala@wikia-inc.com>
2018-09-12 16:51:52 +0200

    Load all translations

--- src/assets/locales/i18n.js
+++ src/assets/locales/i18n.js
@@ -1,15 +1,25 @@
-// import de from './de/main.json';
+import de from './de/main';
 import en from './en/main';
-// import es from './es/main.json';
-// import fr from './fr/main.json';
-// import it from './it/main.json';
-// import ja from './ja/main.json';
-// import pl from './pl/main.json';
-// import pt from './pt/main.json';
-// import ru from './ru/main.json';
-// import zh from './zh/main.json';
-// import zhHans from './zh-hans/main.json';
+import es from './es/main';
+import fr from './fr/main';
+import it from './it/main';
+import ja from './ja/main';
+import pl from './pl/main';
+import pt from './pt/main';
+import ru from './ru/main';
+import zh from './zh/main';
+import zhHant from './zh-hant/main';
 
 export default {
+    de,
     en,
+    es,
+    fr,
+    it,
+    ja,
+    pl,
+    pt,
+    ru,
+    zh,
+    'zh-hant': zhHant,
 };
