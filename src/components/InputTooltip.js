e59f9ce9d0564ed8e57d57a248b3a042c2f48fb9
xkxd <krzysztof.derek@gmail.com>
2018-11-30 12:23:07 +0100

    IW-1174 | add stopPropagation to tooltipinput

--- src/components/InputTooltip.js
+++ src/components/InputTooltip.js
@@ -236,6 +236,7 @@ class InputTooltip extends Component {
         return (
             <div
                 className={cls('pe-input-tooltip wds-dropdown wds-no-chevron', shouldShowDropdown && 'wds-is-active')}
+                onClick={event => event.stopPropagation()}
             >
                 <div className={cls('wds-input', isLinkInvalid && 'has-error')}>
                     <div className="wds-input__field-wrapper">
